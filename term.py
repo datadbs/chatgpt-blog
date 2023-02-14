a9d420e18841c72a0e700bbf47dd9850d9bd9f0d
datadbs <dj9034kr@gmail.com>
2023-02-14 20:28:06 +0900

    feat: add termcrawler

--- term.py
+++ term.py
@@ -1,2 +1,26 @@
-# link = '//*[@id="wrap-pop"]/div[1]/div[3]/div[1]/div/ul/li'
 
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import numpy as np
+
+url = 'https://securities.miraeasset.com/hki/hki3028/r01.do'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    html = response.text
+    soup = BeautifulSoup(html)
+    ul = soup.select_one('ul.result')
+    titles = ul.select('li')
+    pgrr=[]
+    for title in titles:
+        pgr=[]
+        pgr.append(title.get_text().replace('\r\n','').rstrip())
+        pgrr.append(pgr)
+else : 
+    print(response.status_code)
+
+print(pgrr)
+df = pd.DataFrame.from_records(pgrr)
+df.to_excel('term.xlsx')
